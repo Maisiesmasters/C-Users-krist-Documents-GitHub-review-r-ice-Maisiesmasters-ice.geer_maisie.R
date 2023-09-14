b60ea0d66c13bc907297f1baae44d0444c946ec4
Maisiesmasters <143895527+Maisiesmasters@users.noreply.github.com>
2023-09-13 21:52:10 -0600

    Add files via upload

---
+++ ice.geer_maisie.R
@@ -0,0 +1,40 @@
+hometown <- 'whitefish'
+i <- '641945'
+t <- '23'
+u <- '10'
+j <- 't + u'
+print(j)
+a <- b <- q <- r <- 'conflict'
+
+x = 0
+j = 7
+
+comparison = function(arg1, arg2) {
+  t = 3
+  u = 5
+  v = 0
+  if(arg1 > arg2) {
+    v = t * u
+    result_text = paste('arg1 is greater than arg2 and ', v)
+  } else {
+    v = t + u
+    result_text = paste('arg1 is not greater than arg2 and ', v)
+  }
+  return(result_text)
+}
+
+z = comparison(x, j)
+
+print(z)
+
+if(v > 10) {
+  result_text = paste('v is greater than 10')
+} else {
+  result_text = paste('v is less than 10')
+}
+lunches = c('chicken','soy sauce','sugar','sesame oil')
+for (i in lunches) {
+print(i)}
+{if(i = 'sugar') 
+break}
+
